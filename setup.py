8ec7d9d7d5b9d89458015dbab4abd40b363c0b27
Erik Reid <erik.reid@geant.org>
2020-03-17 07:38:06 +0100

    renamed to home-sensu-checks

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='home-checks',
+    name='home-sensu-checks',
     version="0.01",
     author='Erik Reid',
     author_email='merikreid@gmail.com',
-    description='Custom checks for use with Sensu',
+    description='Custom checks/metrics for use with Sensu',
     url=('https://github.com'),
     packages=find_packages(),
     install_requires=[
