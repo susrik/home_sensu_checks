47661f26630ba46887044905d59997327e580fc7
Erik Reid <erik.reid@geant.org>
2020-03-17 08:05:26 +0100

    added pypi classifiers

--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
 from setuptools import setup, find_packages
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name='home-sensu-checks',
-    version="0.01",
+    version='0.0.1',
     author='Erik Reid',
     author_email='merikreid@gmail.com',
     description='Custom checks/metrics for use with Sensu',
-    url=('https://github.com'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url=('https://github.com/susrik/home_sensu_checks'),
     packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta'
+    ],
+    python_requires='>=3.6',
     install_requires=[
         'click',
         'fritzconnection',
